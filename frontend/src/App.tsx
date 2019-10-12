120135ed273f4231aa742cbb5502e22db95b2afe
Shakib Hossain <shakib609@gmail.com>
2019-10-12 13:05:40 +0600

    Initialize React app to serve as Front-End for our app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Django-Redis-Blog</h1>
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+    </div>
+  );
+};
+
+export default App;
