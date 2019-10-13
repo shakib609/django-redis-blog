1efdf8ab745c7c09ccb8e4f74d02faa420bf9db9
Shakib Hossain <shakib609@gmail.com>
2019-10-13 21:02:49 +0600

    Implement initial authReducer and add redux-thunk middleware to store

---
+++ frontend/src/reducers/authReducer.js
@@ -0,0 +1,33 @@
+import { LOGIN, LOGIN_SUCCESS, LOGIN_ERROR, LOGOUT } from '../constants';
+
+const defaultState = {
+  accessToken: null,
+  refreshToken: null,
+  loading: false,
+  errors: null
+};
+
+function authReducer(state = defaultState, action) {
+  switch (action.type) {
+    case LOGOUT:
+      return { ...defaultState };
+    case LOGIN:
+      return { ...state, loading: true };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        accessToken: action.payload.token,
+        accessToken: action.payload.refresh,
+        loading: false
+      };
+    case LOGIN_ERROR:
+      return {
+        ...state,
+        loading: false,
+        errors: action.payload.errors
+      };
+  }
+  return state;
+}
+
+export default authReducer;
