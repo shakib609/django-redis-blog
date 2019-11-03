87b3b7426ee5316d578cbfe591d0abe8c7477e48
Shakib Hossain <shakib609@gmail.com>
2019-11-03 16:20:22 +0600

    Refactor Home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/core';
-import { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
+import { Fragment, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import Button from 'react-bulma-components/lib/components/button';
 import Columns from 'react-bulma-components/lib/components/columns';
 import Section from 'react-bulma-components/lib/components/section';
@@ -9,86 +9,79 @@ import Loader from 'react-bulma-components/lib/components/loader';
 
 import PostList from 'components/PostList';
 import TagList from 'components/TagList';
+import { useThunkDispatch } from 'utils';
 import { fetchPosts } from 'actions/postsAction';
 import { fetchTags } from 'actions/tagsActions';
 import LoadingIndicator from 'components/LoadingIndicator';
 
-class Home extends Component {
-  componentDidMount() {
-    const { fetchPosts, fetchTags, posts, tags } = this.props;
-    if (posts.count === null) fetchPosts(1);
-    if (tags.count === null) fetchTags();
-  }
+const Home = () => {
+  const posts = useSelector(state => state.posts);
+  const tags = useSelector(state => state.tags);
+  const dispatch = useThunkDispatch();
 
-  render() {
-    const { fetchPosts, posts, tags } = this.props;
-    const { next } = posts;
-    let pageNum = null;
-
-    if (next !== null) {
-      const url = new URL(next);
-      pageNum = url.searchParams.get('page');
+  useEffect(() => {
+    if (posts.count === null && tags.count === null) {
+      dispatch(fetchPosts(1));
+      dispatch(fetchTags(1));
     }
+  }, [posts.count, tags.count, dispatch]);
 
-    return (
-      <Section>
-        <Columns>
-          <Columns.Column size="three-quarters">
-            <h3 className="is-size-3 has-text-weight-bold">Recent Posts</h3>
-            {posts.loading && posts.length === 0 ? (
-              <LoadingIndicator />
-            ) : (
-              <Fragment>
-                <PostList posts={posts.results} />
-                {next !== null && (
-                  <p className="has-text-centered">
-                    <Button
-                      css={css`
-                        margin-top: 1.5rem;
-                      `}
-                      onClick={() => fetchPosts(pageNum)}
-                    >
-                      {posts.loading ? (
-                        <Loader
-                          css={css`
-                            border-color: #00c4a7;
-                            border-top-color: transparent;
-                            border-right-color: transparent;
-                          `}
-                        />
-                      ) : (
-                        'Load More'
-                      )}
-                    </Button>
-                  </p>
-                )}
-              </Fragment>
-            )}
-          </Columns.Column>
-          <Columns.Column>
-            {tags.loading ? (
-              <Fragment>
-                <h4 className="has-text-weight-bold">Tags</h4>
-                <LoadingIndicator />
-              </Fragment>
-            ) : (
-              <TagList tags={tags.results} />
-            )}
-          </Columns.Column>
-        </Columns>
-      </Section>
-    );
+  const postsNext = posts.next;
+  let pageNum = null;
+
+  if (postsNext !== null) {
+    const url = new URL(postsNext);
+    pageNum = url.searchParams.get('page');
   }
-}
 
-const mapStateToProps = state => ({ posts: state.posts, tags: state.tags });
+  return (
+    <Section>
+      <Columns>
+        <Columns.Column size="three-quarters">
+          <h3 className="is-size-3 has-text-weight-bold">Recent Posts</h3>
+          {posts.loading && posts.length === 0 ? (
+            <LoadingIndicator />
+          ) : (
+            <Fragment>
+              <PostList posts={posts.results} />
+              {postsNext !== null && (
+                <p className="has-text-centered">
+                  <Button
+                    css={css`
+                      margin-top: 1.5rem;
+                    `}
+                    onClick={() => dispatch(fetchPosts(pageNum))}
+                  >
+                    {posts.loading ? (
+                      <Loader
+                        css={css`
+                          border-color: #00c4a7;
+                          border-top-color: transparent;
+                          border-right-color: transparent;
+                        `}
+                      />
+                    ) : (
+                      'Load More'
+                    )}
+                  </Button>
+                </p>
+              )}
+            </Fragment>
+          )}
+        </Columns.Column>
+        <Columns.Column>
+          {tags.loading ? (
+            <Fragment>
+              <h4 className="has-text-weight-bold">Tags</h4>
+              <LoadingIndicator />
+            </Fragment>
+          ) : (
+            <TagList tags={tags.results} />
+          )}
+        </Columns.Column>
+      </Columns>
+    </Section>
+  );
+};
 
-const mapDispatchToProps = dispatch => ({
-  fetchPosts: page => dispatch(fetchPosts(page)),
-  fetchTags: () => dispatch(fetchTags(1))
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
+export default Home;
